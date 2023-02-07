76ddbb389251d27ad8aeca0a8b963ebe43f20072
TinyTuna <tinytunaaa@gmail.com>
2023-02-07 08:02:15 -0600

    Something essential

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ const express = require('express')
 const server = express()
 
 server.use((req, res) => {
-  res.send('Arson')
+  console.log(req.ip)
+
+  const ips = ["::1", "::ffff:192.168.178.192", "::ffff:192.168.178.24"];
+  if (ips.includes(req.ip)) {
+    res.send("welcome to my candle shop")
+
+  } else {
+    res.send("arson")
+
+
+  }
+  // res.send('Arson')
 })
 
 const PORT = process.env.PORT || 3000
